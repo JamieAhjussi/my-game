64f63e8e5ad20edd9ed6fd13e8222a0e96639a41
James <jamespond707@gmail.comm>
2025-12-25 20:16:15 +0700

    refactor: Migrate Vite React project from TypeScript to JavaScript.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <h1 className="text-headline-1 text-(--color-brand-green)">
+        My First React App on Vite
+      </h1>
+    </section>
+  );
+}
+
+export default App;
