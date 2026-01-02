7cd7dcc27ceb9f7c30fb4277f443bdc484a8cce3
James <jamespond707@gmail.comm>
2026-01-02 17:32:37 +0700

    feat: Initialize project with Tailwind CSS and shadcn/ui, including configuration, utilities, and initial components.

--- src/components/contents/HeroSection.jsx
+++ src/components/contents/HeroSection.jsx
@@ -2,7 +2,7 @@ function HeroSection () {
     return (
        <main className="container px-4 py-8 lg:py-16 mx-auto">
       <div className="flex flex-col lg:flex-row items-center">
-        <div className="lg:w-1/3 mb-8 lg:mb-0 lg:pr-8">
+        <div className="lg:w-[30%] mb-8 lg:mb-0 lg:pr-8">
           <h1 className="text-4xl lg:text-5xl font-bold mb-4">
             Stay <br/>
             Informed, <br/>
@@ -18,7 +18,7 @@ function HeroSection () {
           alt="No picture yet"
           className="h-[530px] object-cover rounded-lg shadow-lg lg:w-1/3 mx-4 mb-8 lg:mb-0"
         />
-        <div className="lg:w-1/3 lg:pl-8">
+        <div className="lg:w-[30%] lg:pl-8">
           <h2 className="text-xl font-semibold mb-2">-Author</h2>
           <h3 className="text-2xl font-bold mb-4">Thompson P.</h3>
           <p className="text-gray-500 mb-4">
