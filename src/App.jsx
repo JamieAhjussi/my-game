d623a0bb0b2e60152b1ea854229a71236491b25e
James <jamespond707@gmail.comm>
2026-01-22 10:10:55 +0700

    feat: set up initial React project structure, routing, markdown rendering, and Tailwind CSS.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import Layout from './components/layout/Layout'
-import Button from './components/ui/Button'
 import NavBar from './components/contents/NavBar'
 import HeroSection from './components/contents/HeroSection'
 import Footer from './components/contents/Footer'
