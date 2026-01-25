e9dbeb1b00813e2527a596c4cb858305beb4118d
James <jamespond707@gmail.comm>
2026-01-25 22:40:36 +0700

    feat: Implement a `ViewPost` component to display blog posts with content, author bio, social sharing, and comments.

--- src/components/contents/ViewPost.jsx
+++ src/components/contents/ViewPost.jsx
@@ -8,7 +8,9 @@ import {
   Copy,
   Loader2,
 } from "lucide-react";
-import authorImage from "../../assets/author-image.png";
+import { comments } from "@/data/comments";
+import { Textarea } from "@/components/ui/textarea";
+import JamieP from "../../assets/JamieP.jpg";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
@@ -207,14 +209,14 @@ function AuthorBio() {
       <div className="flex items-center mb-4">
         <div className="w-16 h-16 rounded-full overflow-hidden mr-4">
           <img
-            src={authorImage}
+            src={JamieP}
             alt="Thompson P."
             className="object-cover w-16 h-16"
           />
         </div>
         <div>
           <p className="text-sm">Author</p>
-          <h3 className="text-2xl font-bold">Thompson P.</h3>
+          <h3 className="text-2xl font-bold">Jamie P.</h3>
         </div>
       </div>
       <hr className="border-gray-300 mb-4" />
